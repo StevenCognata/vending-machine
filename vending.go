cffc350bdb2f96789c3c73254249a4c0dd3348f6
StevenCognata <stevencognata@gmail.com>
2022-08-16 13:36:03 -0400

    added comments for better readability

--- vending.go
+++ vending.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func main() {
 
+	// storing beverage strings in an array
 	var beverages = [10]string{"Dr.Pepper", "Coke", "Iced Tea", "Sprite", "Lemonade", "Water", "RedBull", "Orange Soda", "Iced Coffee", "Seltzer"}
 
+	// printing array items to console
 	for i := 0; i < len(beverages); i++ {
 		fmt.Println(beverages[i])
 	}
